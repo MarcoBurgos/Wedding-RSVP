d87cc1de8425877bfd1f2636b24bcb3e4f50f95b
marcoburgos <marko_burgos@gmail.com>
2019-03-12 01:41:36 -0400

    ajax register

--- weddingRegistry/users/views.py
+++ weddingRegistry/users/views.py
@@ -1,4 +1,4 @@
-from flask import render_template,request,Blueprint, redirect, url_for, flash
+from flask import render_template,request,Blueprint, redirect, url_for, flash, jsonify
 from flask_mail import Mail
 from weddingRegistry import db
 from flask_login import login_required, current_user
@@ -35,34 +35,45 @@ def add_guest():
     return render_template('add_guest.html', form=form)
 
 
+
 @users.route('/register', methods=['GET','POST'])
 def register():
     form = addUserForm()
-    user = User.query.filter_by(email=form.email.data).first()
-    if form.validate_on_submit():
 
-        if user and not user.password_hash:
-            pass_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
-            user.password_hash = generate_password_hash(pass_string)
-
-            mail_body = f"""
-            <p>¡Bienvenido! Gracias por registrarte, por favor usa esta contraseña para iniciar sesión y confirmar tu asistencia a la boda:</p>
-            <p>Tu contraseña es <strong> {pass_string} </strong></p>
-            <p>Sigue esta liga y completa tu confirmación</p>
-            <p><a href="http://127.0.0.1:5000/">Boda Angie & Marco </a></p>
-            <br>
-            <p>¡Gracias y nos vemos el 17 de noviembre!</p>
-            """
-            print(f"user.email is {user.email}")
-            send_email(user.email, "Contraseña para Boda Angie & Marco", mail_body)
-
-            db.session.commit()
-
-            flash(f"Enviamos un correo al mail {form.email.data} con la contraseña. Si no lo recibiste, checa la carpeta de spam o contacta a marko.burgos@gmail.com", 'flash-success')
-
-        elif user and user.password_hash:
-            flash(f"Este correo {form.email.data} ya cuanta con una contraseña asignada. Para evitar spam, solo se genera una vez, contacta a marko.burgos@gmail.com para generar otra, en caso de que no cuentes con tu contaseña.", 'flash-error')
-        else:
-            flash(f"El correo {form.email.data} que ingresaste no existe en la base de datos. Intenta con otro o contacta al administrador: marko.burgos@gmail.com", 'flash-error')
 
     return render_template('register.html', form=form)
+
+
+
+@users.route('/background_process', methods=['POST'])
+def background_process():
+
+    form = addUserForm()
+
+    email = request.form['email']
+
+
+    user = User.query.filter_by(email=form.email.data).first()
+
+    if user and not user.password_hash:
+        pass_string = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+        user.password_hash = generate_password_hash(pass_string)
+
+        mail_body = f"""
+        <p>¡Bienvenido! Gracias por registrarte, por favor usa esta contraseña para iniciar sesión y confirmar tu asistencia a la boda:</p>
+        <p>Tu contraseña es <strong> {pass_string} </strong></p>
+        <p>Sigue esta liga y completa tu confirmación</p>
+        <p><a href="http://127.0.0.1:5000/">Boda Angie & Marco </a></p>
+        <br>
+        <p>¡Gracias y nos vemos el 17 de noviembre!</p>
+        """
+        print(f"user.email is {user.email}")
+        send_email(user.email, "Contraseña para Boda Angie & Marco", mail_body)
+
+        db.session.commit()
+        return jsonify(result='success')
+
+    elif user and user.password_hash:
+        return jsonify(result='repeated')
+    else:
+        return jsonify(result='not-found')
