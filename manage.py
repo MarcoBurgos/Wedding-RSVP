f03dcb8ae442852fe29910119a373d9e34b906d2
marcoburgos <marko_burgos@gmail.com>
2019-04-07 02:05:25 -0400

    local

--- manage.py
+++ manage.py
@@ -4,11 +4,10 @@ from flask_migrate import Migrate, MigrateCommand
 
 from weddingRegistry import app, db
 
-MIGRATION_DIR = os.path.join('weddingRegistry', 'migrations')
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
-migrate = Migrate(app, db, directory=MIGRATION_DIR)
+migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
